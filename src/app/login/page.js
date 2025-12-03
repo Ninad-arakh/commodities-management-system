341894a9611037ee396ed0975f96706d77164e67
Ninad-arakh <arakhninad@gmail.com>
2025-12-03 18:03:05 +0530

    login page validation changes

--- src/app/login/page.js
+++ src/app/login/page.js
@@ -90,7 +90,7 @@ const page = () => {
   };
 
   return (
-    <div className="w-full box-border h-screen flex items-center border">
+    <div className="w-full box-border h-screen flex items-center">
       {/* React Toastify Container */}
       <ToastContainer
         position="bottom-right"
@@ -110,9 +110,9 @@ const page = () => {
       <div className="flex w-full h-full">
         <div className="md:w-7/12 w-full flex justify-center items-center">
           {/* Form Card Wrapper */}
-          <div className="border border-white/25 md:w-7/12 w-11/12 h-[60dvh] mx-auto flex flex-col text-center py-3 px-5 rounded-3xl -mt-[10vh]">
-            <h1 className="text-3xl font-bold">Welcome Back</h1>
-            <h3 className="text-lg">Sign Up For Free</h3>
+          <div className=" md:w-7/12 w-11/12  mx-auto flex flex-col text-center py-3 px-10  rounded-3xl">
+            <h1 className="text-4xl font-bold">Welcome Back</h1>
+            <h3 className="text-sm mt-3">Sign Up For Free</h3>
 
             {/* Form Section */}
             <form
@@ -123,7 +123,7 @@ const page = () => {
               className="flex flex-col gap-4"
             >
               {/* Email Input */}
-              <div className="text-left flex flex-col gap-1">
+              <div className="text-left text-sm flex flex-col gap-1">
                 <label>Email</label>
                 <input
                   type="text"
@@ -136,7 +136,7 @@ const page = () => {
               </div>
 
               {/* Password Input */}
-              <div className="text-left flex flex-col gap-1">
+              <div className="text-left text-sm flex flex-col gap-1">
                 <label>Password</label>
                 <input
                   type="password"
@@ -149,7 +149,7 @@ const page = () => {
               </div>
 
               {/* Terms Checkbox */}
-              <label className="cursor-pointer text-left flex gap-2 items-center">
+              <label className="cursor-pointer text-left text-sm flex gap-2 items-center">
                 <input
                   type="checkbox"
                   checked={isChecked}
@@ -159,24 +159,26 @@ const page = () => {
               </label>
 
               {/* Submit Button */}
-              <button className="w-full rounded-2xl bg-[#8044fe] py-2 text-white cursor-pointer">
+              <button className="w-full text-sm rounded-2xl bg-[#8044fe] py-2 text-white cursor-pointer">
                 Get Started
               </button>
             </form>
 
-            <h2 className="font-medium text-sm mx-auto my-4">OR</h2>
+            <h2 className="font-medium text-xs mx-auto my-4">OR</h2>
 
             {/* Social Login Buttons */}
-            <div className="flex flex-col gap-3">
-              <button className="w-full border border-gray-500/40 rounded-2xl py-2 cursor-pointer flex justify-center items-center gap-1">
+            <div className="flex flex-col gap-4">
+              <button className="w-full text-sm border border-gray-500/40 rounded-2xl py-2 cursor-pointer flex justify-center items-center gap-1">
                 <Image src={google} alt="google png" className="w-6 h-6" /> Sign
                 in with Google
               </button>
 
-              <button className="w-full border border-gray-500/40 rounded-2xl py-2 cursor-pointer flex justify-center items-center gap-1">
+              <button className="w-full text-sm border border-gray-500/40 rounded-2xl py-2 cursor-pointer flex justify-center items-center gap-1">
                 <Image src={facebook} alt="facebook png" className="w-6 h-6" />
                 Sign in with Facebook
               </button>
+
+              <h4 className="text-sm">Already have an account? <span className="text-blue-500 cursor-pointer">Login</span></h4>
             </div>
           </div>
         </div>
